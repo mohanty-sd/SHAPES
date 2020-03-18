acdf6fbc4cba521fe138daa1b6f700189cb2789c
Soumya Mohanty <Soumya@Soumyas-Mac-Pro.rgv.rr.com>
2020-03-18 16:26:47 -0500

    First import.

---
+++ pathset.m
@@ -0,0 +1,5 @@
+%% Set paths
+% Set the path to PSO codes (cloned from https://github.com/mohanty-sd/SDMBIGDAT19)
+addpath ../CRCBOOK/SDMBIGDAT19/CODES/
+% Set the path to data generation codes
+addpath ./DATA
